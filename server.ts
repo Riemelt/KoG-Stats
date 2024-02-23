d364be221618fa3d3149d1e59da38eccb1f7aaab
Riemelt <n.lychenkov@gmail.com>
2024-02-24 06:36:22 +0700

    test changes

--- server.ts
+++ server.ts
@@ -1,37 +1,37 @@
-import * as fs from 'fs';
+//import * as fs from 'fs';
 
 import { fetchMapsData } from './src/scripts/fetchMapsData';
 import { execSync } from 'child_process';
 
 const ONE_MINUTE = 60 * 1000;
-const MINUTES = 10;
+const MINUTES = 30;
 
-const readTestJson = () => {
-  try {
-    const jsonData = fs.readFileSync('./src/data/test.json', 'utf8');
-    const test = JSON.parse(jsonData);
-    return test;
-  } catch (error) {
-    console.log(error);
-    throw new Error('Cannot read test.json');
-  }
-};
-
-const saveTestJson = (bob: number[]) => {
-  const result = {
-    bob,
-    date: new Date(),
-    data: 'test',
-  };
-
-  try {
-    const json = JSON.stringify(result);
-    fs.writeFileSync('./src/data/test.json', json);
-    console.log('test.json saved');
-  } catch (error) {
-    console.log(`saveJson error: ${error}`);
-  }
-};
+//const readTestJson = () => {
+//  try {
+//    const jsonData = fs.readFileSync('./src/data/test.json', 'utf8');
+//    const test = JSON.parse(jsonData);
+//    return test;
+//  } catch (error) {
+//    console.log(error);
+//    throw new Error('Cannot read test.json');
+//  }
+//};
+//
+//const saveTestJson = (bob: number[]) => {
+//  const result = {
+//    bob,
+//    date: new Date(),
+//    data: 'test',
+//  };
+//
+//  try {
+//    const json = JSON.stringify(result);
+//    fs.writeFileSync('./src/data/test.json', json);
+//    console.log('test.json saved');
+//  } catch (error) {
+//    console.log(`saveJson error: ${error}`);
+//  }
+//};
 
 const process = async () => {
   await fetchMapsData();
